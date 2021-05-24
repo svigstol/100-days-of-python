e9d623b4d78c3b340a1743ebc455e0e72bfa22ed
svigstol <sarah.vigstol@gmail.com>
2021-05-24 18:50:39 -0500

    combine encode and decode functions into one

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -4,7 +4,7 @@
 # 5/23/21
 
 # list of letters a through z
-alphabet = list(map(chr, range(97, 122)))
+alphabet = list(map(chr, range(97, 122))) + list(map(chr, range(97, 122)))
 
 print("What would you like to do?")
 print("Type 'encode' to encrypt or 'decode' to decrypt.")
@@ -25,25 +25,49 @@ userInput = input("> ").lower()
 print("Enter the shift number: ")
 shiftNumber = int(input("> "))
 
-def encrypt(plainText, shift):
-    encodedText = ""
-    for letter in plainText:
-        listPosition = alphabet.index(letter)
-        newPosition = listPosition + shift
-        newLetter = alphabet[newPosition]
-        encodedText += newLetter
-    print(f"The encoded message is: {encodedText}")
+def caesar(userInput, shiftNumber):
+    if userAction == 'encode':
+        plainText = userInput
+        shift = shiftNumber
+        encodedText = ""
+        for letter in plainText:
+            listPosition = alphabet.index(letter)
+            newPosition = listPosition + shift
+            newLetter = alphabet[newPosition]
+            encodedText += newLetter
+        print(f"The encoded message is: {encodedText}")
+    elif userAction == 'decode':
+        encodedText = userInput
+        shift = shiftNumber
+        decodedText = ""
+        for letter in encodedText:
+            listPosition = alphabet.index(letter)
+            newPosition = listPosition - shift
+            newLetter = alphabet[newPosition]
+            decodedText += newLetter
+        print(f"The decoded message is: {decodedText}")
 
-def decrypt(plainText, shift):
-    decodedText = ""
-    for letter in plainText:
-        listPosition = alphabet.index(letter)
-        newPosition = listPosition - shift
-        newLetter = alphabet[newPosition]
-        decodedText += newLetter
-    print(f"The decoded message is: {decodedText}")
+caesar(userInput, shiftNumber)
 
-if userAction == 'encode':
-    encrypt(plainText=userInput, shift=shiftNumber)
-else:
-    decrypt(plainText=userInput, shift=shiftNumber)
+#def encrypt(plainText, shift):
+#    encodedText = ""
+#    for letter in plainText:
+#        listPosition = alphabet.index(letter)
+#        newPosition = listPosition + shift
+#        newLetter = alphabet[newPosition]
+#        encodedText += newLetter
+#    print(f"The encoded message is: {encodedText}")
+#
+#def decrypt(encodedText, shift):
+#    decodedText = ""
+#    for letter in encodedText:
+#        listPosition = alphabet.index(letter)
+#        newPosition = listPosition - shift
+#        newLetter = alphabet[newPosition]
+#        decodedText += newLetter
+#    print(f"The decoded message is: {decodedText}")
+#
+#if userAction == 'encode':
+#    encrypt(plainText=userInput, shift=shiftNumber)
+#elif userAction == 'decode':
+#    decrypt(encodedText=userInput, shift=shiftNumber)
