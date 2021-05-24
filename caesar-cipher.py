9c91dad75b0166883000d95810d830a948ff076e
svigstol <sarah.vigstol@gmail.com>
2021-05-23 20:47:49 -0500

    add encrypt and decrypt functions

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -2,3 +2,45 @@
 # Day 8 - Caesar Cipher
 # Sarah Vigstol
 # 5/23/21
+
+# list of letters a through z
+alphabet = list(map(chr, range(97, 122)))
+
+print('''What would you like to do?
+Type 'encode' to encrypt or 'decode' to decrypt.''')
+userAction = input('> ').lower()
+
+print('Enter your message: ')
+userInput = input('> ').lower()
+
+print('Enter the shift number: ')
+shiftNumber = int(input('> '))
+
+def encrypt(plainText, shift):
+    encodedText = ''
+    for letter in plainText:
+        listPosition = alphabet.index(letter)
+        newPosition = listPosition + shift
+        newLetter = alphabet[newPosition]
+        encodedText += newLetter
+    print(f'The encoded message is: {encodedText}')
+
+def decrypt(plainText, shift):
+    decodedText = ''
+    for letter in plainText:
+        listPosition = alphabet.index(letter)
+        newPosition = listPosition - shift
+        newLetter = alphabet[newPosition]
+        decodedText += newLetter
+    print(f'The decoded message is: {decodedText}')
+
+
+# f userAction == 'encode':
+#     encrypt(plainText=userInput, shift=shiftNumber)
+# elif userAction == 'decode':
+#     decrypt(plainText=userInput, shift=shiftNumber)
+# else:
+#     print('Invalid input. Please try again.')
+#     print('''What would you like to do?
+#     Type 'encode' to encrypt or 'decode' to decrypt.''')
+#     userAction = input('> ').lower()
