25bbe56044560ad22cb7ebc0b1e81014d1b9e68a
svigstol <sarah.vigstol@gmail.com>
2021-05-24 19:28:18 -0500

    improve user experience

--- caesar-cipher/caesar-cipher.py
+++ caesar-cipher/caesar-cipher.py
@@ -3,43 +3,57 @@
 # Sarah Vigstol
 # 5/23/21
 
-import ascii
-
-# list of letters a through z
-alphabet = list(map(chr, range(97, 122))) + list(map(chr, range(97, 122)))
-
-print(ascii.logo)
-
-print("What would you like to do?")
-print("Type 'encode' to encrypt or 'decode' to decrypt.")
-userAction = input("> ").lower()
-
-print("Enter your message: ")
-userInput = input("> ").lower()
-
-print("Enter the shift number: ")
-shiftNumber = int(input("> "))
-
 def caesar(userInput, shiftNumber):
     if userAction == 'encode':
         plainText = userInput
         shift = shiftNumber
         encodedText = ""
         for letter in plainText:
-            listPosition = alphabet.index(letter)
-            newPosition = listPosition + shift
-            newLetter = alphabet[newPosition]
-            encodedText += newLetter
+            if letter in alphabet:
+                listPosition = alphabet.index(letter)
+                newPosition = listPosition + shift
+                newLetter = alphabet[newPosition]
+                encodedText += newLetter
+            else:
+                encodedText += letter
         print(f"The encoded message is: {encodedText}")
     elif userAction == 'decode':
         encodedText = userInput
         shift = shiftNumber
         decodedText = ""
         for letter in encodedText:
-            listPosition = alphabet.index(letter)
-            newPosition = listPosition - shift
-            newLetter = alphabet[newPosition]
-            decodedText += newLetter
+            if letter in alphabet:
+                listPosition = alphabet.index(letter)
+                newPosition = listPosition - shift
+                newLetter = alphabet[newPosition]
+                decodedText += newLetter
+            else:
+                decodedText += letter
         print(f"The decoded message is: {decodedText}")
 
-caesar(userInput, shiftNumber)
+# list of letters a through z
+alphabet = list(map(chr, range(97, 122))) + list(map(chr, range(97, 122)))
+
+import ascii
+print(ascii.logo)
+
+allDone = False
+while not allDone:
+    print("What would you like to do?")
+    print("Type 'encode' to encrypt or 'decode' to decrypt.")
+    userAction = input("> ").lower()
+
+    print("Enter your message: ")
+    userInput = input("> ").lower()
+
+    print("Enter the shift number: ")
+    shiftNumber = int(input("> "))
+    shiftNumber = shiftNumber % 26
+
+    caesar(userInput, shiftNumber)
+
+    print("Try again? Type 'yes' or 'no' to continue.")
+    retry = input("> ").lower()
+    if retry == "no":
+        allDone = True
+        print("Goodbye!")
