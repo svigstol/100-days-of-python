e42144c116c8ec6511f9f11e1b318b5a5641b069
svigstol <sarah.vigstol@gmail.com>
2021-05-27 19:34:28 -0500

    complete exercise 9.1

--- grading-program.py
+++ grading-program.py
@@ -4,26 +4,28 @@
 # 5/25/21
 
 studentScores = {
-  "Harry": 81,
-  "Ron": 78,
-  "Hermione": 99,
-  "Draco": 74,
-  "Neville": 62,
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
 }
 
 studentGrades = {}
 
 # loop through values
 for score in studentScores:
-	# compare scores
-	if studentScores[score] >= 91:
-	# assign grades
-		print("Outstanding")
-	elif studentScores[score] >= 81 and studentScores[score] <= 90:
-		print("Exceeds Expectations")
-	elif studentScores[score] >= 71 and studentScores[score] <= 80:
-		print("Acceptable")
-	elif studentScores[score] <= 70:
-		print("Fail")
+    # compare scores
+    if studentScores[score] >= 91:
+        # assign grades
+        studentScores[score] = "Outstanding"
+    elif studentScores[score] >= 81 and studentScores[score] <= 90:
+        studentScores[score] = "Exceeds Expectations"
+    elif studentScores[score] >= 71 and studentScores[score] <= 80:
+        studentScores[score] = "Acceptable"
+    elif studentScores[score] <= 70:
+        studentScores[score] = "Fail"
+
+studentGrades = studentScores
 
 print(studentGrades)
