b9c1fd4eea266e2ce8fea977d23e82546a5f43d1
svigstol <sarah.vigstol@gmail.com>
2021-05-31 12:54:51 -0500

    create operator functions

--- calculator-program.py
+++ calculator-program.py
@@ -4,3 +4,19 @@
 # Allow user to perform additional calculators using the result
 # Sarah Vigstol
 # 5/31/21
+
+def add(n1, n2):
+    """Add two numbers together and return the sum"""
+    return n1 + n2
+
+def subtract(n1, n2):
+    """Subtract a number from another and return the difference"""
+    return n1 - n2
+
+def multiply(n1, n2):
+    """Multiply a number by another number and return the product"""
+    return n1 * n2
+
+def divide(n1, n2):
+    """Divide a number by another number and return the quotient"""
+    return n1 / n2
