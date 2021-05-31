e26d1c43323526933351abffc2462e7b73527576
svigstol <sarah.vigstol@gmail.com>
2021-05-31 07:10:02 -0500

    begin day 10.1

---
+++ calculator-program.py
@@ -0,0 +1,6 @@
+# 100 Days of Python
+# Day 10 - Calculator Program
+# Enter two numbers and an operation, then display output.
+# Allow user to perform additional calculators using the result
+# Sarah Vigstol
+# 5/31/21
---
+++ name-formatter.py
@@ -0,0 +1,5 @@
+# 100 Days of Python
+# Day 10.1 - Name Formatter
+# Practice with function outputs
+# Sarah Vigstol
+# 5/31/21
