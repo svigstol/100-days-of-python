67610e91efbb26b7b8b5f18c827690a1272f4448
svigstol <sarah.vigstol@gmail.com>
2021-05-30 20:02:30 -0500

    define function to gather user input and add to dictionary

--- silent-auction.py
+++ silent-auction.py
@@ -1,4 +1,12 @@
 # 100 Days of Python
 # Day 9 - Silent Auction
+# Enter name/bid from multiple users. When finished, display highest bid.
 # Sarah Vigstol
-# 5/25/21
+# 5/30/21
+
+bids = {}
+userName = input("Who is bidding?\n> ")
+userBid = input(f"Hello, {userName}. What would you like to bid?\n> ")
+bids[userName] = userBid
+
+print(bids)
