dd4ea63c432e5b9b4377cb0c8a71a8eb6d0775c3
svigstol <sarah.vigstol@gmail.com>
2021-05-31 18:05:06 -0500

    core functionality

--- calculator-program.py
+++ calculator-program.py
@@ -20,3 +20,34 @@ def multiply(n1, n2):
 def divide(n1, n2):
     """Divide a number by another number and return the quotient"""
     return n1 / n2
+
+calcFinish = False
+
+while not calcFinish:
+    operations = {
+        "+": add,
+        "-": subtract,
+        "*": multiply,
+        "/": divide
+    }
+
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+
+    for symbol in operations:
+        print(symbol)
+
+    operator = input("Choose an operation from the list above: ")
+    calcFunction = operations[operator]
+    answer = calcFunction(num1, num2)
+
+    print(f"{num1} {operator} {num2} = {answer}")
+
+    goAgain = input("Calculate again? Type 'yes' or 'no.' ")
+    if goAgain == "yes":
+        print("Loading...")
+    elif goAgain == "no":
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid input. Try again.")
